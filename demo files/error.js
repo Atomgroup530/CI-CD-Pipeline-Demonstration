b70ea687cdda6acd0e4e4fec7cf7308d843e4ebf
Atomgroup530 <Atomgroup530@users.noreply.github.com>
2022-11-30 00:29:04 +0000

    Prettified Code!

--- demo files/error.js
+++ demo files/error.js
@@ -1,16 +1,16 @@
 export function readChores() {
-    if (localStorage.getItem("ChoresListData") === null) {
-      const chores = {
-        chores: [],
-        archived: [],
-        openChoresCount: 0,
-        closedChoresCount: 0,
-        choresCountId: 1,
-      };
-      localStorage.setItem("ChoresListData", JSON.stringify(chores));
-      return JSON.parse(localStorage.getItem("ChoresListData"));
-    } else {
-      checkDate();
-      return JSON.parse(localStorage.getItem("ChoresListData"));
-    }
+  if (localStorage.getItem("ChoresListData") === null) {
+    const chores = {
+      chores: [],
+      archived: [],
+      openChoresCount: 0,
+      closedChoresCount: 0,
+      choresCountId: 1,
+    };
+    localStorage.setItem("ChoresListData", JSON.stringify(chores));
+    return JSON.parse(localStorage.getItem("ChoresListData"));
+  } else {
+    checkDate();
+    return JSON.parse(localStorage.getItem("ChoresListData"));
   }
+}
